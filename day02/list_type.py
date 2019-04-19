1660287b26e7f653026084604057fdac7f0576d5
倪刚 <494578387@qq.com>
2019-04-19 11:58:09 +0800

    新增 list 查询

---
+++ day02/list_type.py
@@ -0,0 +1,11 @@
+def list_sel(a):
+    print(a[2])
+    print(a[3:6])
+    print(a[1:4])
+    print(a[0:2])
+    print(a[2:])
+
+if __name__ == '__main__':
+    alist = ['a','4','nihao','8','就是','哈哈']
+    b = list_sel(alist)
+    # print(b)
